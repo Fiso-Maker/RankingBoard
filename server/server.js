31a17d3bb401e9b5963de7e69502471d5b477114
Fiso-Maker <youn110158@naver.com>
2022-12-10 01:42:03 +0900

    Elastic Beanstalk 처리

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv");
 const app = express();
 
@@ -12,6 +13,12 @@ const router = express.Router();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/../client/build/index.html"));
+});
 
 app.get("/api/healthCheck", (req, res) => {
     console.log("ServerHealthCheck");
